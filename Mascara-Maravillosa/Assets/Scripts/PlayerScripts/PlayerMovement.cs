e98318cc9695ca041bf5fb7396e6acf09f0c912d
cacaguaton <fernando.martinez@uteca.edu.mx>
2025-07-17 13:46:10 -0600

    Main-Movimiento

--- Mascara-Maravillosa/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Mascara-Maravillosa/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
     //ptivate PlayerAnimation player_Animation;
     private Rigidbody myBody;   //Aqui va el personaje
 
-    private float walk_Speed = 3f;  //Velocidad de movimiento
+    public float walk_Speed = 3f;  //Velocidad de movimiento
     public float z_Speed = 1.5f;
 
-    private float rotation_Y = -90f;
+    private float rotation_Y = 0f;
     private float rotation_Speed = 15f;
+
+
 
     void Awake()
     {
@@ -25,12 +28,33 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-
     }
-    
+
+    void FixedUpdate()
+    {
+        DetectMovement();
+    }
+
     void DetectMovement()
     {
-        myBody.linearVelocity = new Vector3(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walk_Speed),
-         myBody.linearVelocity.y, Input.GetAxisRaw(Axis.VERTICAL_AXIS)*(-z_Speed));
+        myBody.linearVelocity = new Vector3(
+            Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walk_Speed),
+         myBody.linearVelocity.y,
+         Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-z_Speed));
+
+
     }
+
+    void RotatePlayer()
+    {
+        if (Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, -Mathf.Abs(rotation_Y), 0f);
+        }
+        else if (Input.GetAxisRaw(Axis.VERTICAL_AXIS) < 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, -Mathf.Abs(rotation_Y), 0f);
+        }
+    }//ROTATION
+
 }//class
