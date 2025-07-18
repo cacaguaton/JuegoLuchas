87e728e2995c937d007958241a21e2eea52580f3
cacaguaton <fernando.martinez@uteca.edu.mx>
2025-07-18 14:03:22 -0600

    Main-primeraMitad

--- Mascara-Maravillosa/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Mascara-Maravillosa/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using Unity.VisualScripting;
 
 public enum ComboState
 {
     NONE,
-    PUCH_1,
+    PUNCH_1,
     PUNCH_2,
     PUNCH_3,
     KICK_1,
@@ -38,18 +39,87 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         ComboAttacks();
+        ResetComboState();
     }
 
     void ComboAttacks()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
+            if (current_Combo_State == ComboState.PUNCH_3 ||
+                current_Combo_State == ComboState.KICK_1 ||
+                current_Combo_State == ComboState.KICK_2)
+                return;
+
             current_Combo_State++;
+            activateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+            if (current_Combo_State == ComboState.PUNCH_1)
+            {
+                player_Anim.Punch_1();
+            }
+            if (current_Combo_State == ComboState.PUNCH_2)
+            {
+                player_Anim.Punch_2();
+            }
+            if (current_Combo_State == ComboState.PUNCH_3)
+            {
+                player_Anim.Punch_3();
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.X))
         {
-            player_Anim.Kick_1();
+            //if the current combo is punch 3 or kick 2
+            // return meaninf exit because we have no combos to perform
+            if (current_Combo_State == ComboState.KICK_2 ||
+                current_Combo_State == ComboState.PUNCH_3)
+                return;
+
+            //if the current combo state is NONE, or punch1 or punch2
+            // then we  can set current combo state to kick 1 to chain the combo
+            if (current_Combo_State == ComboState.NONE ||
+                current_Combo_State == ComboState.PUNCH_1 ||
+                current_Combo_State == ComboState.PUNCH_2)
+            {
+
+                current_Combo_State = ComboState.KICK_1;
+
+            }
+            else if (current_Combo_State == ComboState.KICK_1)
+            {
+                //move to kick 2
+                current_Combo_State++;
+            }
+
+            activateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+
+            if (current_Combo_State == ComboState.KICK_1)
+            {
+                player_Anim.Kick_1();
+            }
+
+            if (current_Combo_State == ComboState.KICK_2)
+            {
+                player_Anim.Kick_2();
+            }
+
+        }
+    }//Combo attack
+
+    void ResetComboState()
+    {
+        if (activateTimerToReset)
+        {
+            current_Combo_Timer -= Time.deltaTime;
+            if (current_Combo_Timer <= 0)
+            {
+                current_Combo_State = ComboState.NONE;
+
+                activateTimerToReset = false;
+                current_Combo_Timer = default_Combo_Timer;
+            }
         }
     }
 }
